7b32b5739533d5d0351c39a8c6f1c79e98cc70e0
Hammanek <hammanecek@gmail.com>
2024-05-07 12:23:14 +0200

    update

--- run.bat
+++ run.bat
@@ -2,6 +2,7 @@
 echo Select what do you want to run:
 echo 1. Dailies
 echo 2. Teamup
+echo 3. Abyss
 
 set /p choice=Enter your choice: 
 
@@ -9,6 +10,8 @@ if "%choice%"=="1" (
     py -3.9 AutoAFK2.py --dailies
 ) else if "%choice%"=="2" (
     py -3.9 AutoAFK2.py --teamup
+) else if "%choice%"=="3" (
+    py -3.9 AutoAFK2.py --abyss
 ) else (
     echo Invalid choice. Please select a valid option.
 )
