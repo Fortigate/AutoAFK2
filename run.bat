7b62fde34bcd6d5a108839a19508116c6e4739cf
Hammanek <hammanecek@gmail.com>
2024-05-13 20:45:46 +0200

    dream

--- run.bat
+++ run.bat
@@ -5,7 +5,8 @@ echo 2. Teamup
 echo 3. Abyss
 echo 4. Towers
 echo 5. Quests
-echo 6. Test
+echo 6. Dream Realm
+echo 7. Test
 
 set /p choice=Enter your choice: 
 
@@ -20,6 +21,8 @@ if "%choice%"=="1" (
 ) else if "%choice%"=="5" (
     py -3.9 AutoAFK2.py --quest
 ) else if "%choice%"=="6" (
+    py -3.9 AutoAFK2.py --dream
+) else if "%choice%"=="7" (
     py -3.9 AutoAFK2.py --test
 ) else (
     echo Invalid choice. Please select a valid option.
